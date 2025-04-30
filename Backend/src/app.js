d64d0b1000e5672b17e78e1e6c7e131eea27a4c0
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2025-04-30 10:56:27 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,10 +8,9 @@ const app = express();
 app.use(cors({
   origin: process.env.CORS_ORIGIN, 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type','Authorization']
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials: true
 }))
-
-app.options("*",cors());
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
